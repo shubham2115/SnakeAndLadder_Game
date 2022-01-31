9126e0019259f32daa6f069ccf61a133d3d8e6cb
Shubham <s.shrikhande1@gmail.com>
2022-01-31 21:38:18 +0530

    Player position at 0

---
+++ SnakeAndLadder/SnakeAndLadder/Hello1.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+internal class Hello
+{
+    internal void GAME()
+    {
+        throw new NotImplementedException();
+    }
+}
